0d1feaf8cce9dd8e6527b5dc8d8dc6e0bc6e715c
pouya <pouya16@gmail.com>
2020-03-05 21:10:41 +0330

    make the structure
    making first structure of game
    just to do finish the functions

--- src/FekreBekr.java
+++ src/FekreBekr.java
@@ -4,24 +4,53 @@ import java.util.Random;
 public class FekreBekr {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner read = new Scanner(System.in);
 		int i = 1;
 		int a = 0;
 		String s = "";
 		while(i!=-1) {
+			String result = "";
+			String guess = "";
 			a=0;
 			System.out.println("please enter number of iteration:\n");
 			if(read.hasNextInt()) {
 				a = read.nextInt();
 				s = colorMaker(5);
 				System.out.println("s is : " + s + "\n");
+				for(int j=0;j<a;j++) {
+					System.out.print("guess: ");
+					guess = read.next();
+					if(guess.length()==5){
+						if(checkGuessString(guess)) {
+							if(checkAnswer(guess,s)) {
+								System.out.println("answer is right");
+								j=a;
+							}
+						}else {
+							System.out.println("check your color pick we have : (O)range,(Y)ellow,(P)ink,(B)lue,(R)ed,(W)hite,(G)reen,(P)urple");
+							j--;
+						}
+						
+					}else if(guess.length()==1){
+						
+					}else {
+						System.out.println("Your answer should be 0 or 1 or five chars for prediction");
+						j--;
+					}
+				}			
+				
 				System.out.println("press -1 to end or other numbers to retry\n");
+				i = read.nextInt();
 			}else {
 				System.out.println("try to enter correct iteration it should be a Number greater than ZERO\n");
 			}
 		}
 
+	}
+	
+	private static boolean checkGuessString(String guess) {
+		boolean i = false;
+		return i;
 	}
 	
 	
